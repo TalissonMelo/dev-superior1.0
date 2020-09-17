2d8262083f28df00b83c4f51ecc85e0ecd643e46
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-09-17 18:39:17 -0300

    Listando jogos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import { formatDate } from './helpers'
+import './style.css'
+import { RecordsResponse } from './types';
 
-const Records = () => (
-    <h1>Records</h1>
-);
+const BASE_URL = 'http://localhost:8080'
+
+const Records = () => {
+    const [recordResponse, setRecordResponse] = useState<RecordsResponse>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linePerPage=12&page=`).then(response => setRecordResponse(response.data));
+    }, []);
+
+    return (
+        <div className="page-container">
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{ formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
 export default Records;
